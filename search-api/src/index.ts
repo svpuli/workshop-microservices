0e3933fb8e5d23704bedfa2b2b0b6cd41280d216
Joao Tavares <joaon_tavares@outlook.com>
2019-02-12 18:28:24 +0000

    search-api should only log error

--- search-api/src/index.ts
+++ search-api/src/index.ts
@@ -16,9 +16,6 @@ async function init() {
 	app.use(bodyparser());
 	app.use(helmet());
 
-	// health-check route
-	router.get('/health-check', (ctx) => ctx.status = 200);
-
 	router.get('/search', async (ctx: any, _next: any) => {
 		const searchQuery = ctx.query.q;
 
@@ -28,27 +25,33 @@ async function init() {
 
 		const newsUrl = `http://news-api:3500/news?${qs.stringify({ search: searchQuery })}`;
 		const gifsUrl = `http://giphy-api:4000/gifs?${qs.stringify({ search: searchQuery })}`;
+		let newsResponse;
+		let gifsResponse;
 
 		try {
-			const newsResponse = await request(newsUrl, {
+			newsResponse = await request(newsUrl, {
 				method: 'GET',
 				json: true,
 			});
 
-			const gifsResponse = await request(gifsUrl, {
-				method: 'GET',
-				json: true,
-			});
-
-			ctx.body = {
-				success: true,
-				news: newsResponse.searchResults,
-				gifs: gifsResponse.searchResults
-			};
 		} catch (error) {
 			console.log(error);
-			ctx.throw(error.statusCode);
 		}
+
+		try {
+			gifsResponse = await request(gifsUrl, {
+				method: 'GET',
+				json: true,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+
+		ctx.body = {
+			success: true,
+			news: newsResponse ? newsResponse.searchResults : [],
+			gifs: gifsResponse ? gifsResponse.searchResults : []
+		};
 	});
 
 	app.use(router.routes());
@@ -65,4 +68,3 @@ async function init() {
 };
 
 init();
-
