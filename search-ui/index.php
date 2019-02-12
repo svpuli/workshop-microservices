7f59e76bca7c96e1a77b949161b8e81005f65233
Ricardo Silva <ricardo.silva@pipedrive.com>
2019-02-12 11:24:57 +0000

    add first interface draft for news result

--- search-ui/index.php
+++ search-ui/index.php
@@ -1,13 +1,20 @@
 <?php
+    $searchFor = $_GET['search'];
 
-function escapedString($string) {
-    return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
-}
+    function escapedString($string) {
+        return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
+    }
 
-$search = escapedString($_GET['search']);
-$response = '{ "news": [ { "title": "Hello world" }, { "title": "Tavares sucks!" } ] }';
-$data = json_decode($response);
+    if(is_string($searchFor)){
+        $query = http_build_query([
+            'q'=>escapedString($_GET['search'])
+        ]);
 
+        $url = 'http://search-api:5000/search?'.$query;
+
+        $response = file_get_contents($url);
+        $data = json_decode($response);
+    }
 ?>
 
 <html>
@@ -20,7 +27,7 @@ $data = json_decode($response);
     <body>
         <div class="container">
             <nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
-                <a class="navbar-brand" href="#">My News Center</a>
+                <a class="navbar-brand" href="#">Search Center</a>
             </nav>
 
             <form class="mb-3">
@@ -30,19 +37,38 @@ $data = json_decode($response);
                 <button type="submit" class="btn btn-primary">Search</button>
             </form>
 
-            <?php if (!$search): ?>
-                <p>Enter a search term to search</p>
-            <?php endif; ?>
+            <div class ="container">
+                <div class="row">
 
-            <?php if ($search && $data): ?>
-                <h3>News</h3>
+                    <?php if (!$searchFor): ?>
+                        <p>Enter a search term to search</p>
+                    <?php endif; ?>
+                    
+                    <!-- Card news -->
+                    <?php if ($searchFor && $data): ?>
+                        <?php foreach($data->news as $news): ?>
+                            <div class="col-sm-4 py-2">
+                                <div class="card" style="height:45%!important;">
+                                    <img src="<?php print $news->urlToImage;?>" style="height: 200px;" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h4 class="card-title"><?php print $news->source->name; ?></h5>
+                                        <h4 class="card-title"><?php print $news->title; ?></h4>
+                                        <h6 class="card-title"><?php print  date_format(date_create($news->publishedAt), 'd-m-Y'); ?></h6>
+                                        <p class="card-text"> <?php print $news->description; ?> </p>
+                                        <a  href="<?php print $news->url;?>" 
+                                            class="btn btn-primary btn-lg btn-block" 
+                                            style="position:absolute; bottom:10px; width:90%;">
+                                            Read
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                    <!-- Card news -->
 
-                <?php foreach($data->news as $news): ?>
-                    <div>
-                        <?php print $news->title; ?>
-                    </div>
-                <?php endforeach; ?>
-            <?php endif; ?>
+                </div>
+            </div>
         </div>
     </body>
 </html>
